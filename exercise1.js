39cb1765eab316f81e5441e83a5abeefe4dd5847
Caleb Waters <cw@mekanation.com>
2020-05-26 16:07:24 -0500

    Did all of the exercises including bonus.

--- exercise1.js
+++ exercise1.js
@@ -7,6 +7,22 @@
 
 //your code...
 
+function ftoc(ft) {
+    ct = (((ft - 32)*5)/9);
+    console.log(ct);
+   
+}
+function ctof(ft) {
+    ft = (((ct * 9)/5)+32);
+    console.log(ft);
+    
+}
+
+//tests
+ftoc(90);
+ctof(32);
+
+
 
 
 
@@ -19,7 +35,18 @@
 //your code...
 
 
-
+function oldEnoughToVote(age){
+    if(age >= 18){
+        console.log('Yes');
+    }
+    else{
+        console.log('No');
+    }
+}
+//tests
+oldEnoughToVote(23);
+oldEnoughToVote(17);
+oldEnoughToVote(18);
 
 
 
@@ -30,8 +57,14 @@
 
 //your code...
 
-
-
+function stringToArray(sentence){
+    splitter = sentence.split(" ");
+    console.log(splitter);
+    rejoined = splitter.join(" ");
+    console.log(rejoined);
+}
+//test
+stringToArray("The five boxing wizards jump quickly.");
 
 
 
@@ -41,7 +74,12 @@
 // Write a JavaScript function with a function that reverses your telephone number. Use the split() and join() methods from the previous problem as well as the toString() method to convert a number into a string and reverse() method to reverse an array in place.
 
 //your code...
-
+function phoneReversal(phone){
+    phoneToArray = phone.toString(10).split("");
+    reversedPhone = phoneToArray.reverse().join("");
+    console.log(reversedPhone);
+}
+phoneReversal(8174049873);
 
 
 
@@ -52,7 +90,18 @@
 
 //your code...
 
+let Car = function(make,model,year,color){
+    this.make= make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+}
 
+var myCar = new Car('Mazda',"3",2017,'blue');
+function carViewer(car){
+    console.log(car.make,car.model,car.year,car.color);
+}
+carViewer(myCar);
 
 
 
@@ -63,7 +112,18 @@
 
 
 //your code...
-
+function oddOrEven(number){
+    count=0;
+    while(count <= number){
+        if((count % 2) == 0){
+            console.log(count + ": is even");
+        }else{
+            console.log(count + ": is odd");
+        }
+        count++;
+    }
+}
+oddOrEven(15);
 
 
 
@@ -72,7 +132,20 @@
 // Write a JavaScript function that iterates the integers from 1 to 100. For multiples of 3 print "TEK" instead of the number and for multiples of 5, print "camp." For numbers that are multiples of both 3 and 5, print "TEKcamp."
 
 //your code...
-
+function tekFizzBuzz(number){
+    for (i = 0; i <= number; i++){
+        if(((i % 3) == 0) && (((i % 5) == 0))){
+            console.log("TEKcamp");
+        }else if(((i % 3) == 0)) {
+            console.log("TEK");
+        }else if(((i % 5) == 0)){
+            console.log("camp");
+        }else {
+            console.log(i);
+        }
+    }
+}
+tekFizzBuzz(100);
 
 /************************************************************* */
 // Problem 8:
@@ -82,21 +155,84 @@ const nums = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
 // and 9: the zeroth, third, sixth, and ninth values.
 
 //your code...
-
+function arrayElementLogger(array){
+    for(i = 0; i <= array.length; i++){
+        if( (i % 3 == 0) ) {
+            console.log(array[i]);
+        }
+    }
+}
+arrayElementLogger(nums);
 
 
 // Problem 9:
 const foodArray = [ 'potatoes', 'tamales', 'lemon','strawberries','chocolate', 'pudding', {school : 'TEKcamp'} ];
 //access the value of the last element of the array and set it to a variable called school.  print the school variable to the console.
 
+
 const adjectiveArray = [ 'salty', 'spicy', 'sour', 'sweet', 'rich','creamy','amazing'];
 // Using both the foodArray and the adjectiveArray, make a
 // "for" loop that console.log()'s a sentence for each
 // corresponding value in the arrays. Add the word "is" or "are" depending on if the food is singular or plural.  "Potatoes are salty", "Lemon is sour".
+//code
+var school = foodArray[foodArray.length -1].school;
+console.log(school);
+
+//helper function to simplify array loop.
+function pluralityChecker(word){
+    if(word[word.length -1] == 's'){
+        return true;
+    }else {
+        return false;
+    }
+
+}
+
+
+foodArray.forEach((value, index) => {
+    const value2 = adjectiveArray[index];
+    if(value.school !== undefined && (pluralityChecker(value) == false)){
+        console.log(value.school + ' is ' + value2 );
+    }else if(pluralityChecker(value)== false){
+        console.log(value + ' is ' + value2);
+    }else {
+        console.log(value + ' are '+ value2);
+    }
+
+});
+    
+
+    
+
 
 /************************************************************* */
 // Bonus Assignment: Create 4 mathematical function expressions, add,subtract,multiply,divide.  put them in an array, and create a doMath() function that randomly does one of the 4 operations whenever it is run.  the doMath() function should print out what mathetmatical function was carried out.  The doMath() function should return the computed value of any operation performed.
-const operations = [];
-function doMath() {};
+var operations = [
+    add = function(a,b){
+    return a + b; },
+
+    subtract = function(a,b){
+    return  a - b;},
+
+    multiply = function(a,b){
+        return a * b; },
+    
+    divide = function(a,b){
+            return a / b; }
 
 
+];
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+
+function doMath(a,b) {
+   console.log("Function Ran: " + operations[getRandomInt(4)].name + "| Calculation: "+ operations[getRandomInt(4)](a,b));
+   
+    
+    
+};
+doMath(1,2);
+doMath(15,5);
