fea2b7738512a242b78e13d5d4c26e5a6be93349
Caleb Waters <cw@mekanation.com>
2020-05-29 10:15:10 -0500

    working timer

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -1,2 +1,34 @@
 console.log("time to start the timer!");
 
+const start = document.getElementById('start');
+
+const stop = document.querySelector('#stop');
+
+const reset = document.querySelector('#reset');
+const minutes = document.querySelector('#minutes');
+const seconds = document.querySelector('#seconds');
+
+// let minutesConverter = Math.floor(time/60);
+let time = 75;
+
+function counter(){
+    
+    document.getElementById("seconds").innerHTML = secondsToMinutes(++time);
+    // document.getElementById("minutes").innerHtml = Math.floor(time/60);
+}
+
+
+function secondsToMinutes(time) {
+    sec = time % 60;
+    min = parseInt(time / 60);
+    if(sec.toString().length == 1){
+        sec = "0" + sec;
+    }
+    return min + ":" + sec;
+}
+
+function clearSeconds() {
+    time = 0;
+    document.getElementById("seconds").innerHTML = secondsToMinutes(time);
+
+}
